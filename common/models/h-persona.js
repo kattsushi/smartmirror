cd1725ac186d0ff372924a6a498e8619f281782a
Efrain <e.rincon@adelfuns.com.ve>
2016-04-23 18:23:21 -0430

    Creación de hooks y ajustes generales de modelos

--- common/models/h-persona.js
+++ common/models/h-persona.js
@@ -1,3 +1,22 @@
 module.exports = function(HPersona) {
 
+  //Hooks que se encarga de apagar(false) todos los status de un idespejo para quede activo el que esta proximo a crear
+  HPersona.observe('before save', function (ctx, next) {
+    console.log(ctx.instance);
+    console.log(ctx.instance.enlaceId);
+
+    if (ctx.instance){
+      var Persona = ctx.Model.app.models.hPersona;
+      console.log(Persona);
+      Persona.updateAll({enlaceId: ctx.instance.enlaceId},{status: false}).then(function (data) {
+          console.log("hola");
+          console.log(data);
+      },function (err) {
+          console.log(err);
+      });
+
+      next();
+    }
+});
+
 };
