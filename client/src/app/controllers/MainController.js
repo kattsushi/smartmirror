7d4895e5b6e39b7bf9dcacb4f8626ef2b43382b2
Andres <a.jimenez@adelfuns.com.ve>
2016-04-24 12:51:55 -0430

    Merge branch 'master' into test

--- client/src/app/controllers/MainController.js
+++ client/src/app/controllers/MainController.js
@@ -74,21 +74,15 @@
           .position('bottom right')
       );
     }
-  }
-  //----------------------------------------------------------------------------
-  vm.sections = [];
-  Enlace
-     .find({filter:{where:{id_espejo:"001"}}})
-     .$promise
-     .then(function(data){
-        for (var i = 0; i < data.length; i++) {
-           
-        }
-     });
-
-
-
-
-
-
+    //----------------------------------------------------------------------------
+    vm.sections = [];
+    Enlace
+       .find({filter:{where:{id_espejo:"001"}}})
+       .$promise
+       .then(function(data){
+          for (var i = 0; i < data.length; i++) {
+            vm.sections.push(data[i]);
+          }
+       });
+   }
 })();
