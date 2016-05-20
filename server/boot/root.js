9c7015c19fa279a54da9f71339fd7f5029a0e59e
Efrain <e.rincon@adelfuns.com.ve>
2016-05-20 15:25:02 -0400

    creación de servicio para carga de folleto

--- server/boot/root.js
+++ server/boot/root.js
@@ -5,6 +5,7 @@ module.exports = function(server) {
 
   router.get('/', server.loopback.status());
 
+  //Proceso para carga de logo ----------INICIO--------------
   var storage =   multer.diskStorage({
     destination: function (req, file, callback) {
       callback(null, __dirname +'/../../admin/src/assets/img/pictures');
@@ -47,6 +48,52 @@ module.exports = function(server) {
           // console.log(req.files);
       });
   });
+  //Proceso para carga de logo ----------FIN--------------
+
+  //Proceso para carga de volante ----------INICIO--------------
+  var storageHandout =   multer.diskStorage({
+    destination: function (req, file, callback) {
+      callback(null, __dirname +'/../../admin/src/assets/img/handout');
+      // console.log('/image');
+    },
+    filename: function (req, file, callback) {
+      console.log(server.models.enlace.find());
+      var Enlace = server.models.enlace;
+
+      Enlace
+      .find({where:{id_espejo: '001'}})
+      .then(function (data) {
+        var handoutId = data[0].handout + 1;
+        Enlace
+        .updateAll({id_espejo: '001'},{handout:handoutId})
+        .then(function (data) {
+            console.log(data);
+            callback(null, handoutId.toString() +'.png');
+        },function (err) {
+            console.log(err);
+        });
+      }, function (err) {
+        console.log(err);
+      });
+
+    }
+  });
+
+  var uploadHandout = multer({ storageHandout : storageHandout}).single('file');
+
+  router.post('/api/handout',function(req,res){
+
+      uploadHandout(req,res,function(err) {
+          if(err) {
+              return res.end("Error uploading file.");
+          }
+          // res.end("File is uploaded");
+          res.jsonp(req.files);
+          //res.send("Hello World");
+          // console.log(req.files);
+      });
+  });
+  //Proceso para carga de volante ----------FIN--------------
 
   server.use(router);
 };
